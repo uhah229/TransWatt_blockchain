eebab8f9c2f8dc135d93bb189ef828aae29fbb45
david <uhah229@gmail.com>
2019-10-19 19:19:21 -0400

    transfering tokens

--- contracts/DappToken.sol
+++ contracts/DappToken.sol
@@ -6,12 +6,29 @@ contract DappToken {
   string public standard = "TransWatt Token v1.0";
   uint256 public totalSupply;
 
+  event Transfer(
+    address indexed _from,
+    address indexed _to,
+    uint256 _value
+  );
+
   mapping(address => uint256) public balanceOf;
 
   constructor(uint256 _initialSupply) public {
     balanceOf[msg.sender] = _initialSupply;
     totalSupply = _initialSupply;
-    // allocate the initial supply
+  }
+
+  function transfer(address _to, uint256 _value) public returns (bool success) {
+    require(balanceOf[msg.sender] >= _value);
+
+    balanceOf[msg.sender] -= _value;
+    balanceOf[_to] += _value;
+
+    emit Transfer(msg.sender, _to, _value);
+
+    return true;
 
   }
+
 }
