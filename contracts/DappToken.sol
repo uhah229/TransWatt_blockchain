bf1e46fd80afc3c6143403eafa5af0e4222bedab
david <uhah229@gmail.com>
2019-10-19 18:37:45 -0400

    made TransWatt Tokens

--- contracts/DappToken.sol
+++ contracts/DappToken.sol
@@ -1,12 +1,17 @@
 pragma solidity ^0.5.8;
 
 contract DappToken {
-  // Constructor
-  // set the total number of tokens
-  // read the total number of tokens
+  string public name = "TransWatt Token";
+  string public symbol = "TWTT";
+  string public standard = "TransWatt Token v1.0";
   uint256 public totalSupply;
 
-  constructor() public {
-    totalSupply = 1000000;
+  mapping(address => uint256) public balanceOf;
+
+  constructor(uint256 _initialSupply) public {
+    balanceOf[msg.sender] = _initialSupply;
+    totalSupply = _initialSupply;
+    // allocate the initial supply
+
   }
 }
